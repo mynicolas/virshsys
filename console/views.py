4ec733350680890f76f2fd69e182a356345befd9
Nicolas(H) <nicolas.lshuang@gmail.com>
2014-12-28 23:06:14 +0800

    modify console/views.py

--- console/views.py
+++ console/views.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
+from django.shortcuts import Http404
+from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponseRedirect
+from os import popen
+
+
